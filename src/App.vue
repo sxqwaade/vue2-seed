04727cd3833415bb99c0184d1bc3519427162086
sxqwaade <sxqwade@sina.com>
2017-05-08 13:53:47 +0800

    ipdate

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-content">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "styles/app.scss";
+
+</style>
